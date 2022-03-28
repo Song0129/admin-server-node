8c2c413537859a54d078ec20b1c7034b5ffafe6e
Song0129 <Song_3191@163.com>
2022-03-28 14:55:36 +0800

    update

--- server.js
+++ server.js
@@ -5,24 +5,24 @@
   说明: 只有当连接上数据库后才去启动服务器
 3. 使用中间件
  */
-const mongoose = require('mongoose');
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express(); // 产生应用对象
-const path = require('path');
+const path = require("path");
 
 // 声明使用静态中间件
-app.use('/admin', express.static(path.join(__dirname, 'public')));
+app.use("/admin", express.static(path.join(__dirname, "public")));
 // 声明使用解析post请求的中间件
 app.use(express.urlencoded({ extended: true })); // 请求体参数是: name=tom&pwd=123
 app.use(express.json()); // 请求体参数是json结构: {name: tom, pwd: 123}
 // 声明使用解析cookie数据的中间件
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 // 声明使用路由器中间件
-const indexRouter = require('./routers');
-app.use('/', indexRouter); //
+const indexRouter = require("./routers");
+app.use("/", indexRouter); //
 
-const fs = require('fs');
+const fs = require("fs");
 
 // 必须在路由器中间之后声明使用
 // app.use((req, res) => {
@@ -46,14 +46,15 @@ app.use(a);
 
 // 通过mongoose连接数据库
 mongoose
-	.connect('mongodb://106.15.231.186:27077/myAdmin', { useNewUrlParser: true })
+	// 使用时需要修改为自己的数据库地址  账户密码
+	.connect("mongodb://xxx.xxx.xxx.xxx:xxxx/myAdmin", { useNewUrlParser: true })
 	.then(() => {
-		console.log('连接数据库成功!!!');
+		console.log("连接数据库成功!!!");
 		// 只有当连接上数据库后才去启动服务器
-		app.listen('7777', () => {
-			console.log('服务器启动成功, 请访问: http://localhost:7777');
+		app.listen("7777", () => {
+			console.log("服务器启动成功, 请访问: http://localhost:7777");
 		});
 	})
 	.catch(error => {
-		console.error('连接数据库失败', error);
+		console.error("连接数据库失败", error);
 	});
